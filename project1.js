1b52d4c9cede5aff5e44799c77888673f6ffd179
dimashermanto <40769069+dimashermanto@users.noreply.github.com>
2019-10-27 18:49:13 +0700

    Add files via upload
    My first commit

---
+++ project1.js
@@ -0,0 +1,64 @@
+var scrollreveal = ScrollReveal();
+
+scrollreveal.reveal('.jumbotron-content', {
+    delay: 300,
+    origin: 'bottom',
+    distance: '70px',
+    duration: 1500,
+    reset: true
+});
+
+scrollreveal.reveal('.info-panel', {
+    delay: 400,
+    reset: true,
+    origin: 'bottom',
+    distance: '50px',
+    duration: 1500
+});
+
+scrollreveal.reveal('.working-space', {
+    delay: 300,
+    reset: true,
+    origin: 'right',
+    distance: '60px',
+    duration: 1000
+});
+
+scrollreveal.reveal('.logo', {
+    delay: 300,
+    origin: 'bottom',
+    distance: '50px',
+    reset: true,
+    duration: 1000
+});
+
+
+// Fucking smooth scrolling
+$(document).ready(function () {
+
+    $(".scroll").click(function (e) {
+        e.preventDefault();
+
+        $('body, html').animate({
+            scrollTop: $(this.hash).offset().top
+        }, 1300);
+
+    });
+
+    $(".scroll-bottom").click(function (e) {
+        e.preventDefault();
+
+        $('body, html').animate({
+            scrollTop: $(this.hash).offset().top
+        }, 1300);
+
+    });
+
+});
+
+
+$(document).ready(function () {
+    $(".jumbotron").parallaxie({
+        speed: 0.5
+    });
+});
